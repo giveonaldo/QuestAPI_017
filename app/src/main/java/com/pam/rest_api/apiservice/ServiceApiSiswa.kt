cb1e23e175e91688aed6d1b6f2f27810755e4c46
Uknown <abriansyahadam5@gmail.com>
2025-09-02 16:50:23 +0700

    ubah route

--- app/src/main/java/com/pam/rest_api/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/pam/rest_api/apiservice/ServiceApiSiswa.kt
@@ -4,7 +4,6 @@ import com.pam.rest_api.modeldata.DataSiswa
 import retrofit2.http.GET
 
 interface ServiceApiSiswa {
-    @GET("http://127.0.0.1:8000/api/identitas")
-
+    @GET("/api/identitas")
     suspend fun getAllSiswa(): List<DataSiswa>
 }
