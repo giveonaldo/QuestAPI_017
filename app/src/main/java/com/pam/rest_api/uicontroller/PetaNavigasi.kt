35e721121d8aee53d621a248e3e4a35d2624b34c
giveonaldo <abriansyahadam5@gmail.com>
2025-09-09 15:11:54 +0700

    menambah route di peta navigasi

--- app/src/main/java/com/pam/rest_api/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/pam/rest_api/uicontroller/PetaNavigasi.kt
@@ -3,11 +3,16 @@ package com.pam.rest_api.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.pam.rest_api.uicontroller.route.DestinasiDetail
+import com.pam.rest_api.uicontroller.route.DestinasiEdit
 import com.pam.rest_api.uicontroller.route.DestinasiEntry
 import com.pam.rest_api.uicontroller.route.DestinasiHome
+import com.pam.rest_api.view.DetailSiwaScreen
 import com.pam.rest_api.view.EntrySiswaScreen
 import com.pam.rest_api.view.HomeScreen
 
@@ -30,6 +35,13 @@ fun HostNavigasi(
 
         composable(DestinasiEntry.route) {
             EntrySiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)})
+        }
+
+        composable(DestinasiDetail.routeWithArgs, arguments = listOf(navArgument(DestinasiDetail.itemIdArg) {
+            type = NavType.IntType
+        })) {
+            DetailSiwaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit.route}/$it")},
+                navigateBack = { navController.navigate(DestinasiHome.route) })
         }
     }
 }
