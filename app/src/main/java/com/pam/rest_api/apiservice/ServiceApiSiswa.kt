0963a9ee12b43122ce02d6794e8323ab83f0623e
giveonaldo <abriansyahadam5@gmail.com>
2025-09-09 16:00:52 +0700

    fix route

--- app/src/main/java/com/pam/rest_api/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/pam/rest_api/apiservice/ServiceApiSiswa.kt
@@ -7,6 +7,7 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface ServiceApiSiswa {
     @GET("api/identitas")
@@ -16,11 +17,11 @@ interface ServiceApiSiswa {
     suspend fun postDataSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 
     @GET("api/identitas/get/{id}")
-    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+    suspend fun getSatuSiswa(@Path("id") id: Int): DataSiswa
 
     @PUT("api/identitas/edit/{id}")
     suspend fun editSatuSiswa(@Query("id") id:Int, @Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 
-    @DELETE()
-    suspend fun hapusSatuSiswa(@Query("id") id:Int): retrofit2.Response<Void>
+    @DELETE("api/identitas/{id}")
+    suspend fun hapusSatuSiswa(@Path("id") id:Int): retrofit2.Response<Void>
 }
