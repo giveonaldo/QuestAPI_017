9d76de7869216824509ca120dd6864f28bb02f6c
giveonaldo <abriansyahadam5@gmail.com>
2025-09-09 14:07:27 +0700

    membuat body detail data siswa

--- app/src/main/java/com/pam/rest_api/view/HalamanDetail.kt
+++ app/src/main/java/com/pam/rest_api/view/HalamanDetail.kt
@@ -1,12 +1,16 @@
 package com.pam.rest_api.view
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.res.dimensionResource
@@ -42,7 +46,18 @@ fun DetailSiwaScreen(
                 },
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(dimensionResource(R.dimen.padding_large))
-            ) { }
-        }
-    ) {  }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.btn_edit)
+                )
+            }
+        }, modifier = modifier
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+
+    }
 }
+
+@Composable
+private fun BodyDetailDataSiswa() {}
