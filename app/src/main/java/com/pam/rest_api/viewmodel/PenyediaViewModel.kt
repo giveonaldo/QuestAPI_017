2779a8c83d3faa3dd1c8267d908c5c70311228ae
giveonaldo <abriansyahadam5@gmail.com>
2025-09-09 15:11:25 +0700

    menambah detail view model

--- app/src/main/java/com/pam/rest_api/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/pam/rest_api/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.pam.rest_api.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -13,5 +14,7 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDatasiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDatasiswa) }
+        initializer { DetailViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositoryDatasiswa) }
+        // EditViewModel
     }
 }
